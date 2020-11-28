b0115c7ddfa8336c766a2e97b20679f0f4bdc58b
Saher Elsayed <saher.suga@gmail.com>
2020-11-27 18:49:02 -0600

    fixing the structure of the board

--- baby.hs
+++ baby.hs
@@ -5,12 +5,17 @@ structure:
            [7,8,9]]
 It wouldn't work that way
 -}
+-- [Open 1,Open 2,Open 3,Open 4,Open 5,Open 6,Open 7,Open 8,Open 9]
+
 data Part
     = Open Int
     | Player Char
-      deriving (Show,Eq)
-      
---[Open 1,Open 2,Open 3,Open 4,Open 5,Open 6,Open 7,Open 8,Open 9]
+    deriving Eq
 
+instance Show Part where
+    show (Open n) = show n
+    show (Player c) =[c]
+
+    
 main::IO()
 main=undefined
